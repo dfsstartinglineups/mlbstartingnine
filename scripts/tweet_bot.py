e052a17a355a3fb766fe81a886ffda56ab2ba3aa
dfsstartinglineups <fofreepicks@gmail.com>
2026-04-12 05:07:27 -0700

    Update tweet_bot.py

--- scripts/tweet_bot.py
+++ scripts/tweet_bot.py
@@ -880,7 +880,7 @@ FUTBOL_LEAGUES = {
     61:  {"name": "🇫🇷 LIGUE 1", "tag": "#Ligue1", "url_slug": "ligue1", "base_url": "https://futbolstartingeleven.com/ligue1.html"}, 
     10:  {"name": "🌎 INTERNATIONAL Friendlies", "tag": "#Friendly", "url_slug": "intl", "base_url": "https://futbolstartingeleven.com/friendlies.html"},
     39:  {"name": "\U0001f3f4\U000e0067\U000e0062\U000e0065\U000e006e\U000e0067\U000e007f PREMIER LEAGUE", "tag": "#EPL", "url_slug": "epl", "base_url": "https://futbolstartingeleven.com/epl.html"},
-    140: {"name": "🇪🇸 LA LIGA", "tag": "#LaLiga", "url_slug": "laliga", "x_client": laliga_client, "v1_client": laliga_api_v1, "base_url": "https://futbolstartingeleven.com/laliga.html"},
+    140: {"name": "🇪🇸 LA LIGA", "tag": "#LaLiga", "url_slug": "laliga", "base_url": "https://futbolstartingeleven.com/laliga.html"},
     135: {"name": "SERIE A 🇮🇹", "tag": "#SerieA", "url_slug": "seriea", "x_client": seriea_client, "v1_client": seriea_api_v1, "base_url": "https://futbolstartingeleven.com/seriea.html"},
     2:   {"name": "🇪🇺 CHAMPIONS LEAGUE", "tag": "#UCL", "url_slug": "ucl"},
     45:  {"name": "\U0001f3f4\U000e0067\U000e0062\U000e0065\U000e006e\U000e0067\U000e007f FA CUP", "tag": "#FACup", "url_slug": "facup"},
